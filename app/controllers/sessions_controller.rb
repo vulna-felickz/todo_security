34a47e58b90a4226518d77c7e1d7d5d3fcafbf4e
Chad Bentz <1760475+felickz@users.noreply.github.com>
2024-04-05 17:47:17 -0400

    add user object filter from user input

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,6 +23,21 @@ class SessionsController < ApplicationController
     render :new
   end
 
+  def createFilter
+    @user = User.first(conditions: ["login = ?", auth_params[:login]])
+    if @user
+      @current_user = @user
+      session[:user_id] = @user.id
+      redirect_to root_url
+      return true
+    elsif User.find_by(login: auth_params[:login])
+      flash.now[:alert] = "Incorrect password!"
+    else
+      flash.now[:alert] = "User not found!"
+    end
+    render :new
+  end
+
   def destroy
     session[:user_id] = nil
     reset_session
